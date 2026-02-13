1a1d9eb734dd982558575c14ac050f987a928fd2
synapes620 <74415396+synapes620@users.noreply.github.com>
2026-02-13 23:10:25 +0900

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,23 @@
+POST /board/view.php?&bdId=goodsreview&sno=271 HTTP/2
+Host: chosimmall.co.kr
+Cookie: GD5SESSID=j4e5jc617mud61hstivh1kruviritrkqv45t8sdh1jarsojq2h1qvrf74gcrhenql8uubk61q07fklq89mefvbgmgdoi9lf8uslg1d1; _fwb=68zSSAYrF0B7sooqGsLXKY.1769133567475; recentKeyword=%5B%22%5C%22+OR+1%3D1+--%5E%7C%5E2026.01.23%22%2C%22%27+OR+1%3D1+--%5E%7C%5E2026.01.23%22%5D; firstVisitInfo=%257B%2522event_time%2522%253A%25222026-01-25T12%253A21%253A10%252B09%253A00%2522%252C%2522referrer%2522%253A%2522https%253A%255C%252F%255C%252Fwww.chosimstudy.co.kr%255C%252F%2522%252C%2522uri%2522%253A%2522https%253A%255C%252F%255C%252Fwww.chosimmall.co.kr%253A443%255C%252F%2522%257D; suid=135215cd39a94ea2952d288e286986bd; wcs_bt=s_22b5316d9494:1770295688
+Content-Length: 22
+Cache-Control: max-age=0
+Sec-Ch-Ua: "Not(A:Brand";v="8", "Chromium";v="144"
+Sec-Ch-Ua-Mobile: ?0
+Sec-Ch-Ua-Platform: "macOS"
+Accept-Language: ko-KR,ko;q=0.9
+Origin: https://chosimmall.co.kr
+Content-Type: application/x-www-form-urlencoded
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Sec-Fetch-Site: same-origin
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Referer: https://chosimmall.co.kr/board/list.php?bdId=goodsreview&sno=271
+Accept-Encoding: gzip, deflate, br
+Priority: u=0, i
+
+writerPw=1123123
